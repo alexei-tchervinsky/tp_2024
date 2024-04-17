3fc4c4515b288374868898fa1131cf5549c9fefc
4eburekGit <kottsovdan@gmail.com>
2024-04-17 07:43:26 +0300

    ver.1.1.2-acceptance-fix

--- kottsov.daniil/T2/main.cpp
+++ kottsov.daniil/T2/main.cpp
@@ -5,23 +5,19 @@ int main()
   std::vector<lst::Data> data;
   std::string inputLine;
   std::getline(std::cin, inputLine);
-  while (inputLine!="0" && inputLine!="stop")
+  std::istringstream iss(inputLine);
+  try
   {
-    std::istringstream iss(inputLine);
-    try
-    {
-      std::copy(
-        std::istream_iterator<lst::Data>(iss),
-        std::istream_iterator<lst::Data>(),
-        std::back_inserter(data)
-      );
-    }
-    catch (const lst::BadInput& err)
-    {
-      std::cerr << err.what() << "\n";
-      return 1;
-    }
-    std::getline(std::cin,inputLine);
+    std::copy(
+      std::istream_iterator<lst::Data>(iss),
+      std::istream_iterator<lst::Data>(),
+      std::back_inserter(data)
+    );
+  }
+  catch (const lst::BadInput& err)
+  {
+    std::cerr << err.what() << "\n";
+    return 1;
   }
   std::stable_sort(data.begin(), data.end(), lst::DataComparator());
   std::cout << "Data:\n";
