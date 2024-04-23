c2087edba293dc55f9644d370add9aa4b117b7dc
Виктор Евстигнеев <victorevstigneev36@gmail.com>
2024-04-23 14:35:33 +0300

    Try t2

--- evstigneev.victor/T2/datastruct.hpp
+++ evstigneev.victor/T2/datastruct.hpp
@@ -5,57 +5,57 @@
 
 namespace evstigneev
 {
-	struct DataStruct
-	{
-		unsigned long long key1;
-		char key2;
-		std::string key3;
-	};
+  struct DataStruct
+  {
+    unsigned long long key1;
+    char key2;
+    std::string key3;
+  };
 
-	struct ULLOctIO
-	{
-		unsigned long long& ull;
-	};
+  struct ULLOctIO
+  {
+    unsigned long long& ull;
+  };
 
-	struct DelimiterIO
-	{
-		char delimiter;
-	};
+  struct DelimiterIO
+  {
+    char delimiter;
+  };
 
-	struct LabelIO
-	{
-		std::string str;
-	};
+  struct LabelIO
+  {
+    std::string str;
+  };
 
-	struct StringIO
-	{
-		std::string& str;
-	};
+  struct StringIO
+  {
+    std::string& str;
+  };
 
-	struct CharLitIO
-	{
-		char& chr;
-	};
+  struct CharLitIO
+  {
+    char& chr;
+  };
 
-	std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-	std::istream& operator>>(std::istream& in, ULLOctIO&& dest);
-	std::istream& operator>>(std::istream& in, StringIO&& dest);
-	std::istream& operator>>(std::istream& in, CharLitIO&& dest);
-	std::istream& operator>>(std::istream& in, LabelIO&& dest);
-	std::istream& operator>>(std::istream& in, DataStruct& dest);
-	std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
-	bool operator<(const DataStruct& a, const DataStruct& b);
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, ULLOctIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, CharLitIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+  bool operator<(const DataStruct& a, const DataStruct& b);
 
-	class iofmtguard
-	{
-	public:
-		iofmtguard(std::basic_ios<char>& s);
-		~iofmtguard();
-	private:
-		std::basic_ios<char>& s_;
-		char fill_;
-		std::streamsize precision_;
-		std::basic_ios<char>::fmtflags fmt_;
-	};
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios<char>& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios<char>& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags fmt_;
+  };
 }
 #endif
--- evstigneev.victor/T2/main.cpp
+++ evstigneev.victor/T2/main.cpp
@@ -8,22 +8,22 @@
 
 int main()
 {
-	using namespace evstigneev;
-	std::vector< DataStruct > data;
-	while (!std::cin.eof())
-	{
-		std::copy(
-			std::istream_iterator< DataStruct >{std::cin},
-			std::istream_iterator< DataStruct >{},
-			std::back_inserter(data)
-		);
-	}
-	std::cout << "Data:\n";
-	std::sort(data.begin(), data.end());
-	std::copy(
-		std::begin(data),
-		std::end(data),
-		std::ostream_iterator< DataStruct >(std::cout, "\n")
-	);
-	return 0;
+  using namespace evstigneev;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< DataStruct >{std::cin},
+      std::istream_iterator< DataStruct >{},
+      std::back_inserter(data)
+    );
+  }
+  std::cout << "Data:\n";
+  std::sort(data.begin(), data.end());
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+  return 0;
 }
