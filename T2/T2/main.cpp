61f5ae98ecd7ea82734865317bf75098a612b7ec
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-03 19:43:09 +0300

    init

---
+++ T2/T2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "data_struct.h"
+
+int main() {
+    std::vector<DataStruct> dataVector;
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        try {
+            DataStruct data;
+            std::istringstream iss(line);
+            iss >> data;
+            dataVector.push_back(data);
+        }
+        catch (...) {
+            continue;
+        }
+    }
+    std::sort(dataVector.begin(), dataVector.end());
+
+    for (const auto& data : dataVector) {
+        std::cout << data << std::endl;
+    }
+
+    return 0;
+}
+
+
+//Doing the test
+//(:key1 10.5d : key2 20ll : key3 "Hello" : )
+//(:key1 5.0d : key2 15ll : key3 "World" : )
+//(:key1 8.75d : key2 10ll : key3 "This is a test" : )
