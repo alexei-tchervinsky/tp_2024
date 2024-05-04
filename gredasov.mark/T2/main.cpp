b9cfd71b1f02fa50080753cc04a37916f8d6d0f5
Mark Gredasov <markgredasov@iMac.local>
2024-05-04 12:02:11 +0300

    Removed whitespaces from main.cpp

--- gredasov.mark/T2/main.cpp
+++ gredasov.mark/T2/main.cpp
@@ -19,7 +19,7 @@ int main()
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
-  std::sort(data.begin(), data.end()); 
+  std::sort(data.begin(), data.end());
   std::copy(std::begin(data), std::end(data), std::ostream_iterator< DataStruct >(std::cout, "\n"));
 
   return 0;
