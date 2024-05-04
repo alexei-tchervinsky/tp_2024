584bc107b7c69e7dd818444b1327f4a93d690974
Mark Gredasov <markgredasov@iMac.local>
2024-05-04 11:54:04 +0300

    CG fix

--- gredasov.mark/T2/main.cpp
+++ gredasov.mark/T2/main.cpp
@@ -8,29 +8,20 @@
 
 int main()
 {
-	using namespace gredasov;
-	std::vector< DataStruct > data;
-	while (!std::cin.eof())
-	{
-		std::copy(
-			std::istream_iterator< DataStruct >{std::cin},
-			std::istream_iterator< DataStruct >{},
-			std::back_inserter(data)
-		);
-		if (std::cin.fail())
-		{
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-		}
-	}
-	std::sort(data.begin(), data.end());
-    
-	std::copy(
-		std::begin(data),
-		std::end(data),
-		std::ostream_iterator< DataStruct >(std::cout, "\n")
-	);
+  using namespace gredasov;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(std::istream_iterator< DataStruct >{std::cin}, std::istream_iterator< DataStruct >{}, std::back_inserter(data));
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end()); 
+  std::copy(std::begin(data), std::end(data), std::ostream_iterator< DataStruct >(std::cout, "\n"));
 
-	return 0;
+  return 0;
 }
 
