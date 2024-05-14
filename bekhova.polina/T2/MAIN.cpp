755dc05611643d0ab8b2b1927f2c7559a7ca4bb1
Полина Бехова <behova.pa@edu.spbstu.ru>
2024-05-14 15:30:21 +0300

    T2 fix

--- bekhova.polina/T2/MAIN.cpp
+++ bekhova.polina/T2/MAIN.cpp
@@ -8,27 +8,27 @@
 
 int main()
 {
-    using namespace bekhova;
-    std::vector< DataStruct > data;
-    while (!std::cin.eof())
-    {
-        std::copy(
-            std::istream_iterator< DataStruct >{std::cin},
-            std::istream_iterator< DataStruct >{},
-            std::back_inserter(data)
-        );
-        if (std::cin.fail())
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-        }
-    }
-    std::sort(data.begin(), data.end());
+  using namespace bekhova;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
     std::copy(
-        std::begin(data),
-        std::end(data),
-        std::ostream_iterator< DataStruct >(std::cout, "\n")
+      std::istream_iterator< DataStruct >{std::cin},
+      std::istream_iterator< DataStruct >{},
+      std::back_inserter(data)
     );
-    return 0;
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+  return 0;
 }
 
--- bekhova.polina/T2/datastruct.h
+++ bekhova.polina/T2/datastruct.h
@@ -5,50 +5,50 @@
 
 namespace bekhova
 {
-    struct DataStruct
-    {
-        unsigned long long key1;
-        unsigned long long key2;
-        std::string key3;
-    };
+  struct DataStruct
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
 
-    struct ULLOctIO
-    {
-        unsigned long long& ull;
-    };
-    struct ULLHexIO
-    {
-        unsigned long long& ref;
-    };
-    struct DelimiterIO
-    {
-        char delimiter;
-    };
+  struct ULLOctIO
+  {
+  h  unsigned long long& ull;
+  };
+  struct ULLHexIO
+  {
+    unsigned long long& ref;
+  };
+  struct DelimiterIO
+  {
+    char delimiter;
+  };
 
-    struct StringIO
-    {
-        std::string& str;
-    };
+  struct StringIO
+  {
+    std::string& str;
+  };
 
-    std::istream& operator>>(std::istream& in, DelimiterIO&& d);
-    std::istream& operator>>(std::istream& in, ULLOctIO&& d);
-    std::istream& operator>>(std::istream& in, ULLHexIO&& d);
-    std::istream& operator>>(std::istream& in, StringIO&& d);
-    std::istream& operator>>(std::istream& in, DataStruct& d);
-    std::ostream& operator<<(std::ostream& out, const DataStruct& d);
-    bool operator<(const DataStruct& a, const DataStruct& b);
+  std::istream& operator>>(std::istream& in, DelimiterIO&& d);
+  std::istream& operator>>(std::istream& in, ULLOctIO&& d);
+  std::istream& operator>>(std::istream& in, ULLHexIO&& d);
+  std::istream& operator>>(std::istream& in, StringIO&& d);
+  std::istream& operator>>(std::istream& in, DataStruct& d);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& d);
+  bool operator<(const DataStruct& a, const DataStruct& b);
 
-    class iofmtguard
-    {
-    public:
-        explicit iofmtguard(std::basic_ios< char >& s);
-        ~iofmtguard();
-    private:
-        std::basic_ios< char >& s_;
-        char fill_;
-        std::streamsize precision_;
-        std::basic_ios< char >::fmtflags fmt_;
-    };
+  class iofmtguard
+  {
+  public:
+    explicit iofmtguard(std::basic_ios< char >& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char >& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
 }
 #endif
 
