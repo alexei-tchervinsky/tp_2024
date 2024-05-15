dcdfe28782a729610b5d555be4bb8e6fd71b206b
RofLeR8 <159780758+RofLeR8@users.noreply.github.com>
2024-05-15 11:13:20 +0300

    10

--- kabalin.aleksey/T2/main.cpp
+++ kabalin.aleksey/T2/main.cpp
@@ -1,3 +1,4 @@
+#include "Compare.hpp"
 #include "DataStruct.hpp"
 #include <algorithm>
 #include <iostream>
@@ -5,28 +6,20 @@
 #include <limits>
 #include <vector>
 
-int main()
-{
-  using kabalin::DataStruct;
-  std::vector<DataStruct> data;
+int main() {
+  std::vector<kabalin::DataStruct> data_struct;
   while (!std::cin.eof())
   {
-    std::copy(
-      std::istream_iterator<DataStruct>{std::cin},
-      std::istream_iterator<DataStruct>{},
-      std::back_inserter(data)
-    );
-    if (std::cin.fail())
+    std::copy(std::istream_iterator<kabalin::DataStruct>(std::cin),
+    std::istream_iterator<kabalin::DataStruct>(),
+    std::back_inserter(data_struct));
+    if (std::cin.fail() == true)
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
-  std::sort(data.begin(), data.end());
-  std::copy(
-    std::begin(data),
-    std::end(data),
-    std::ostream_iterator<DataStruct>(std::cout, "\n")
-  );
+  std::stable_sort(data_struct.begin(), data_struct.end(), kabalin::Compare());
+  std::copy(data_struct.cbegin(), data_struct.cend(),std::ostream_iterator<kabalin::DataStruct>{std::cout, "\n"});
   return 0;
 }
