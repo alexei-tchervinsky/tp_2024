b9e70919b8fee18a8aa53d703a193ee29ffe06c5
RofLeR8 <159780758+RofLeR8@users.noreply.github.com>
2024-05-15 03:56:10 +0300

    8

--- kabalin.aleksey/T2/Guard.hpp
+++ kabalin.aleksey/T2/Guard.hpp
@@ -6,15 +6,14 @@ namespace kabalin
 {
   class iofmtguard
   {
-    public:
-      iofmtguard(std::basic_ios<char> &s);
-      ~iofmtguard();
-
-    private:
-      std::basic_ios<char> &s_;
-      char fill_;
-      std::streamsize precision_;
-      std::basic_ios<char>::fmtflags fmt_;
+  public:
+    explicit iofmtguard(std::basic_ios< char > &s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
   };
 }
 #endif
