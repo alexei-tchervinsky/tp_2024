6feae47de46f0aad1b1a6848a32a6e4631416d17
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-15 03:43:47 +0300

    adding dataStuct

--- mungoi.shellerv/T2/data_struct.cpp
+++ mungoi.shellerv/T2/data_struct.cpp
@@ -1,40 +1,71 @@
-#include "data_struct.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 #include <sstream>
 #include <iomanip>
 
-std::istream& operator>>(std::istream& is, DataStruct& data) {
-    char c;
-    std::string field;
-    while (is >> c >> field) {
-        if (field == "key1") {
-            double val;
-            is >> val >> c;
-            data.key1 = val;
-        }
-        else if (field == "key2") {
-            long long val;
-            is >> val >> c;
-            data.key2 = val;
-        }
-        else if (field == "key3") {
-            std::getline(is >> std::ws, data.key3, ':');
-        }
-        else {
-            std::string tmp;
-            std::getline(is >> std::ws, tmp, ':');
+struct DataStruct {
+    double key1;
+    long long key2;
+    std::string key3;
+};
+
+std::istream& operator>>(std::istream& is, DataStruct& ds) {
+    std::string line;
+    if (std::getline(is, line)) {
+        std::istringstream iss(line);
+        char colon, bracket, type;
+        if (iss >> bracket >> colon && bracket == '(' && colon == ':') {
+            std::string key;
+            while (iss >> key) {
+                if (key == "key1") {
+                    iss >> ds.key1 >> type >> colon;
+                }
+                else if (key == "key2") {
+                    iss >> ds.key2 >> type >> colon;
+                }
+                else if (key == "key3") {
+                    iss >> std::quoted(ds.key3) >> colon;
+                }
+                if (colon != ':') break;
+            }
         }
     }
     return is;
 }
 
-std::ostream& operator<<(std::ostream& os, const DataStruct& data) {
-    os << "key1: " << std::fixed << std::setprecision(10) << data.key1
-        << " key2: " << data.key2 << " key3: \"" << data.key3 << "\"";
-    return os;
+std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
+    return os << "(:key1 " << ds.key1 << "d:key2 " << ds.key2 << "ll:key3 \"" << ds.key3 << "\":)";
 }
 
-bool operator<(const DataStruct& a, const DataStruct& b) {
-    if (a.getKey1() != b.getKey1()) return a.getKey1() < b.getKey1();
-    if (a.getKey2() != b.getKey2()) return a.getKey2() < b.getKey2();
-    return a.getKey3() < b.getKey3();
+bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
+}
+
+
+int main() {
+    std::vector<DataStruct> data;
+
+    std::string testData = R"(
+    (:key1 45.0d:key2 123ll:key3 "Apple":)
+    (:key1 10.5d:key2 -45ll:key3 "Banana":)
+    (:key1 45.0d:key2 123ll:key3 "Cherry":)
+    (:key1 10.5d:key2 123ll:key3 "Date":)
+    (:key1 45.0d:key2 -123ll:key3 "Fig":)
+    )";
+
+    std::istringstream iss(testData);
+
+    std::copy(std::istream_iterator<DataStruct>(iss),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data));
+
+    std::sort(data.begin(), data.end(), compareDataStruct);
+
+    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
+    return 0;
 }
