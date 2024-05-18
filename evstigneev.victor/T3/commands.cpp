41b678c086a56ce0529b835ebc813bac52a5f566
Виктор Евстигнеев <victorevstigneev36@gmail.com>
2024-05-18 22:31:11 +0300

    fix build 4

--- evstigneev.victor/T3/commands.cpp
+++ evstigneev.victor/T3/commands.cpp
@@ -6,7 +6,7 @@
 #include <numeric>
 #include <functional>
 
-double evstigneev::countArea(Polygon& poly) {
+double evstigneev::countArea(const Polygon& poly) {
   double area = 0.0;
   for (size_t i = 0; i < poly.points.size() - 1; ++i) {
     area += poly.points[i].x * poly.points[i + 1].y - poly.points[i + 1].x * poly.points[i].y;
@@ -15,27 +15,27 @@ double evstigneev::countArea(Polygon& poly) {
   return std::abs(area) / 2.0;
 }
 
-double evstigneev::sumAreaEven(double res, Polygon& poly)
+double evstigneev::sumAreaEven(double res, const Polygon& poly)
 {
   return (poly.points.size() % 2 == 0) ? res + countArea(poly) : res;
 }
 
-double evstigneev::sumAreaOdd(double res, Polygon& poly)
+double evstigneev::sumAreaOdd(double res, const Polygon& poly)
 {
   return (poly.points.size() % 2 != 0) ? res + countArea(poly) : res;
 }
 
-double evstigneev::sumAreas(double res, Polygon& poly)
+double evstigneev::sumAreas(double res, const Polygon& poly)
 {
   return res + countArea(poly);
 }
 
-double evstigneev::sumAreaEqual(double res, Polygon& poly, size_t numOfVexes)
+double evstigneev::sumAreaEqual(double res, const Polygon& poly, size_t numOfVexes)
 {
   return (poly.points.size() == numOfVexes) ? res + countArea(poly) : res;
 }
 
-std::ostream& evstigneev::area(std::istream& in, std::ostream& out, std::vector<Polygon>& poly)
+std::ostream& evstigneev::area(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly)
 {
   std::string cmd = "";
   in >> cmd;
@@ -60,29 +60,29 @@ std::ostream& evstigneev::area(std::istream& in, std::ostream& out, std::vector<
   return out;
 }
 
-double evstigneev::countAreasEven(std::vector <Polygon>& poly)
+double evstigneev::countAreasEven(const std::vector <Polygon>& poly)
 {
   return std::accumulate(poly.cbegin(), poly.cend(), 0, sumAreaEven);
 }
 
-double evstigneev::countAreasOdd(std::vector <Polygon>& poly)
+double evstigneev::countAreasOdd(const std::vector <Polygon>& poly)
 {
   return std::accumulate(poly.cbegin(), poly.cend(), 0, sumAreaOdd);
 }
 
-double evstigneev::countAreasMean(std::vector <Polygon>& poly)
+double evstigneev::countAreasMean(const std::vector <Polygon>& poly)
 {
   double areas = std::accumulate(poly.cbegin(), poly.cend(), 0, sumAreas);
   return areas / poly.size();
 }
 
-double evstigneev::countAreasVexes(std::vector <Polygon>& poly, int numOfVexes)
+double evstigneev::countAreasVexes(const std::vector <Polygon>& poly, int numOfVexes)
 {
   return std::accumulate(poly.cbegin(), poly.cend(), 0,
     std::bind(sumAreaEqual, std::placeholders::_1, std::placeholders::_2, numOfVexes));
 }
 
-std::ostream& evstigneev::max(std::istream& in, std::ostream& out, std::vector<Polygon>& poly)
+std::ostream& evstigneev::max(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly)
 {
   std::string cmd = "";
   in >> cmd;
@@ -97,21 +97,21 @@ std::ostream& evstigneev::max(std::istream& in, std::ostream& out, std::vector<P
   return out;
 }
 
-double evstigneev::maxArea(std::vector<Polygon>& poly)
+double evstigneev::maxArea(const std::vector<Polygon>& poly)
 {
   std::vector<double> area(poly.size());
   std::transform(poly.begin(), poly.end(), area.begin(), countArea);
   return *std::max_element(area.begin(), area.end());
 }
 
-double evstigneev::maxVexes(std::vector<Polygon>& poly)
+double evstigneev::maxVexes(const std::vector<Polygon>& poly)
 {
   std::vector<int> numOfVexes(poly.size());
   std::transform(poly.begin(), poly.end(), numOfVexes.begin(), numOfVexes);
   return *std::max_element(numOfVexes.begin(), numOfVexes.end());
 }
 
-std::ostream& evstigneev::min(std::istream& in, std::ostream& out, std::vector<Polygon>& poly)
+std::ostream& evstigneev::min(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly)
 {
   std::string cmd = "";
   in >> cmd;
@@ -126,14 +126,14 @@ std::ostream& evstigneev::min(std::istream& in, std::ostream& out, std::vector<P
   return out;
 }
 
-double evstigneev::minArea(std::vector<Polygon>& poly)
+double evstigneev::minArea(const std::vector<Polygon>& poly)
 {
   std::vector<double> area(poly.size());
   std::transform(poly.begin(), poly.end(), area.begin(), countArea);
   return *std::min_element(area.begin(), area.end());
 }
 
-double evstigneev::minVexes(std::vector<Polygon>& poly)
+double evstigneev::minVexes(const std::vector<Polygon>& poly)
 {
   std::vector<int> numOfVexes(poly.size());
   std::transform(poly.begin(), poly.end(), numOfVexes.begin(), numOfVexes);
@@ -141,7 +141,7 @@ double evstigneev::minVexes(std::vector<Polygon>& poly)
 }
 
 std::ostream& evstigneev::count(std::istream& in, std::ostream& out,
-  std::vector<Polygon>& poly)
+  const std::vector<Polygon>& poly)
 {
   std::string cmd = "";
   in >> cmd;
@@ -176,17 +176,17 @@ int evstigneev::countPolys(const std::vector <Polygon>& poly, int numOfVexes)
   return std::count_if(poly.begin(), poly.end(), std::bind(numOfVexesEqual, std::placeholders::_1, numOfVexes));
 }
 
-bool evstigneev::vexesEven(Polygon& poly)
+bool evstigneev::vexesEven(const Polygon& poly)
 {
   return poly.points.size() % 2 == 0;
 }
 
-bool evstigneev::vexesOdd(Polygon& poly)
+bool evstigneev::vexesOdd(const Polygon& poly)
 {
   return poly.points.size() % 2 == 1;
 }
 
-bool evstigneev::numOfVexesEqual(Polygon poly, size_t numOfVexes)
+bool evstigneev::numOfVexesEqual(const Polygon poly, size_t numOfVexes)
 {
   return poly.points.size() == numOfVexes;
 }
--- evstigneev.victor/T3/commands.hpp
+++ evstigneev.victor/T3/commands.hpp
@@ -4,31 +4,31 @@
 
 namespace evstigneev
 {
-  std::ostream& area(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  std::ostream& max(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  std::ostream& min(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  std::ostream& count(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  std::ostream& lessArea(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  std::ostream& maxSeq(std::istream& in, std::ostream& out, std::vector<Polygon>& poly);
-  double countArea(Polygon& poly);
-  double sumAreaEven(double res, Polygon& poly);
-  double sumAreaOdd(double res, Polygon& poly);
-  double sumAreas(double res, Polygon& poly);
-  double sumAreaEqual(double res, Polygon& poly, size_t numOfVexes);
-  double countAreasEven(std::vector <Polygon>& poly);
-  double countAreasOdd(std::vector <Polygon>& poly);
-  double countAreasMean(std::vector <Polygon>& poly);
-  double countAreasVexes(std::vector <Polygon>& poly, int numOfVexes);
-  double maxArea(std::vector<Polygon>& poly);
-  double maxVexes(std::vector<Polygon>& poly);
-  double minArea(std::vector<Polygon>& poly);
-  double minVexes(std::vector<Polygon>& poly);
+  std::ostream& area(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  std::ostream& max(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  std::ostream& min(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  std::ostream& count(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  std::ostream& lessArea(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  std::ostream& maxSeq(std::istream& in, std::ostream& out, const std::vector<Polygon>& poly);
+  double countArea(const Polygon& poly);
+  double sumAreaEven(double res, const Polygon& poly);
+  double sumAreaOdd(double res, const Polygon& poly);
+  double sumAreas(double res, const Polygon& poly);
+  double sumAreaEqual(double res, const Polygon& poly, size_t numOfVexes);
+  double countAreasEven(const std::vector <Polygon>& poly);
+  double countAreasOdd(const std::vector <Polygon>& poly);
+  double countAreasMean(const std::vector <Polygon>& poly);
+  double countAreasVexes(const std::vector <Polygon>& poly, int numOfVexes);
+  double maxArea(const std::vector<Polygon>& poly);
+  double maxVexes(const std::vector<Polygon>& poly);
+  double minArea(const std::vector<Polygon>& poly);
+  double minVexes(const std::vector<Polygon>& poly);
   int countEven(const std::vector <Polygon>& poly);
   int countOdd(const std::vector <Polygon>& poly);
   int countPolys(const std::vector <Polygon>& poly, int numOfVexes);
-  bool vexesEven(Polygon& poly);
-  bool vexesOdd(Polygon& poly);
-  bool numOfVexesEqual(Polygon poly, size_t numOfVexes);
+  bool vexesEven(const Polygon& poly);
+  bool vexesOdd(const Polygon& poly);
+  bool numOfVexesEqual(const Polygon poly, size_t numOfVexes);
 }
 
 #endif
