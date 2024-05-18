f291aa123a39d0a094ed8b63249424f2f3d0a8c2
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-18 04:14:40 +0300

    removing long lines

--- mungoi.shellerv/T2/main.cpp
+++ mungoi.shellerv/T2/main.cpp
@@ -58,7 +58,8 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             }
             else if (key == "key2")
             {
-                if (!(iss >> data.key2) || !(iss >> c) || (c != 'l' && c != 'L' || !(iss >> c) || (c != 'l' && c != 'L')))
+                if (!(iss >> data.key2) || !(iss >> c) || (c != 'l' && 
+                    c != 'L' || !(iss >> c) || (c != 'l' && c != 'L')))
                 {
                     in.setstate(std::ios::failbit);
                     return in;
@@ -90,7 +91,8 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& data)
         return out;
     }
 
-    out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1 << "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
+    out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1 << 
+        "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
     return out;
 }
 
