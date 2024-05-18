09ec12972adc68c70251e1b075f6d08335be7537
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-19 00:38:29 +0300

    deleting multi-lines-2

--- mungoi.shellerv/T2/main.cpp
+++ mungoi.shellerv/T2/main.cpp
@@ -6,104 +6,86 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <stdexcept>
 
-struct DataStruct
-{
-
+struct DataStruct {
     double key1;
     long long key2;
     std::string key3;
+};
 
-}
-;
-
-std::istream& operator>>(std::istream& in, DataStruct& data)
-{
-
+std::istream& operator>>(std::istream& in, DataStruct& data) {
     std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
+    if (!sentry) return in;
 
     std::string input;
-    if (!std::getline(in, input, ')'))
-    {
+    if (!std::getline(in, input, ')')) {
         in.setstate(std::ios::failbit);
         return in;
     }
 
     std::istringstream iss(input + ')');
     char c;
-    if (!(iss >> c) || c != '(')
-    {
+    if (!(iss >> c) || c != '(') {
         in.setstate(std::ios::failbit);
         return in;
     }
 
-    while (iss >> c)
-    {
-        if (c == ':')
-        {
+    bool key1_set = false, key2_set = false, key3_set = false;
+    while (iss >> c) {
+        if (c == ':') {
             std::string key;
-            if (!(iss >> key))
-            {
+            if (!(iss >> key)) {
                 in.setstate(std::ios::failbit);
                 return in;
             }
 
-            if (key == "key1")
-            {
-                if (!(iss >> data.key1) || !(iss >> c) || (c != 'd' && c != 'D'))
-                {
+            if (key == "key1") {
+                if (!(iss >> data.key1) || !(iss >> c) || (c != 'd' && c != 'D')) {
                     in.setstate(std::ios::failbit);
                     return in;
                 }
-            }
-            else if (key == "key2")
-            {
-                if (!(iss >> data.key2) || !(iss >> c) || (c != 'l' &&
-                    c != 'L') || !(iss >> c) || (c != 'l' && c != 'L'))
-                {
+                key1_set = true;
+            } else if (key == "key2") {
+                if (!(iss >> data.key2) || !(iss >> c) || (c != 'l' && c != 'L') || !(iss >> c) || (c != 'l' && c != 'L')) {
                     in.setstate(std::ios::failbit);
                     return in;
                 }
-            }
-            else if (key == "key3")
-            {
-                if (!(iss >> std::quoted(data.key3)))
-                {
+                key2_set = true;
+            } else if (key == "key3") {
+                if (!(iss >> std::quoted(data.key3))) {
                     in.setstate(std::ios::failbit);
                     return in;
                 }
+                key3_set = true;
+            } else {
+                in.setstate(std::ios::failbit);
+                return in;
             }
         }
 
-        if (iss.peek() == ')')
-        {
+        if (iss.peek() == ')') {
             break;
         }
+    }
+
+    if (!(key1_set && key2_set && key3_set)) {
+        in.setstate(std::ios::failbit);
     }
 
     return in;
 }
 
-std::ostream& operator<<(std::ostream& out, const DataStruct& data)
-{
+std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
     std::ostream::sentry sentry(out);
-    if (!sentry)
-    {
-        return out;
-    }
+    if (!sentry) return out;
 
-    out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1 <<
-        "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
+    out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1
+        << "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
     return out;
 }
 
-int main()
-{
-
+int main() {
     std::vector<DataStruct> data;
     std::istringstream iss(
         "( :key1 45.0d :key2 123ll :key3 \"Apple\" )\n"
@@ -111,32 +93,33 @@ int main()
         "( :key1 45.0d :key2 123ll :key3 \"Cherry\" )\n"
         "( :key1 10.5d :key2 123ll :key3 \"Date\" )\n"
         "( :key1 45.0d :key2 -123ll :key3 \"Fig\" )\n"
-    )
-;
+    );
 
-    std::copy(
-        std::istream_iterator<DataStruct>(iss),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
-    )
-;
+    try {
+        std::copy(
+            std::istream_iterator<DataStruct>(iss),
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(data)
+        );
 
-    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
-        if (a.key1 != b.key1)
-            return a.key1 < b.key1;
-        if (a.key2 != b.key2)
-            return a.key2 < b.key2;
-        return a.key3.length() < b.key3.length();
+        if (data.empty()) {
+            std::cerr << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+            return 1;
+        }
+
+        std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+            if (a.key1 != b.key1)
+                return a.key1 < b.key1;
+            if (a.key2 != b.key2)
+                return a.key2 < b.key2;
+            return a.key3.length() < b.key3.length();
+        });
+
+        std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 2;
     }
-)
-;
-
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    )
-;
 
     return 0;
 }
