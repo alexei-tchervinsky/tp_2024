811f722a89b0fb5c09a2d84d5e9b45d43a663b76
Полина Бехова <behova.pa@edu.spbstu.ru>
2024-05-20 23:27:22 +0300

    T2 fix 2

---
+++ bekhova.polina/MAIN.cpp
@@ -0,0 +1,33 @@
+﻿#include "datastruct.h"
+#include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+  using namespace bekhova;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+    std::istream_iterator< DataStruct >{std::cin},
+    std::istream_iterator< DataStruct >{},
+    std::back_inserter(data)
+    );
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+  return 0;
+}
