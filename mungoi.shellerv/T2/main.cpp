afb6537f75f226c314dd95cc0e483851dc9c19d1
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 01:53:48 +0300

    defining operators >> and << on same way-6

--- mungoi.shellerv/T2/main.cpp
+++ mungoi.shellerv/T2/main.cpp
@@ -1,45 +1,52 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "data_struct.h"
+
 int main()
 {
-std::vector<DataStruct> data;
-std::istringstream iss(
-"( :key1 45.0d :key2 123ll :key3 "Apple" )\n"
-"( :key1 10.5d :key2 -45ll :key3 "Banana" )\n"
-"( :key1 45.0d :key2 123ll :key3 "Cherry" )\n"
-"( :key1 10.5d :key2 123ll :key3 "Date" )\n"
-"( :key1 45.0d :key2 -123ll :key3 "Fig" )\n"
-);
-
-bool foundValidRecord = false;
-
-std::copy_if(
-    std::istream_iterator<DataStruct>(iss),
-    std::istream_iterator<DataStruct>(),
-    std::back_inserter(data),
-    [&foundValidRecord](const DataStruct&) {
-        foundValidRecord = true;
-        return true;
+    std::vector<DataStruct> data;
+    std::istringstream iss(
+        "( :key1 45.0d :key2 123ll :key3 "Apple" )\n"
+        "( :key1 10.5d :key2 -45ll :key3 "Banana" )\n"
+        "( :key1 45.0d :key2 123ll :key3 "Cherry" )\n"
+        "( :key1 10.5d :key2 123ll :key3 "Date" )\n"
+        "( :key1 45.0d :key2 -123ll :key3 "Fig" )\n"
+    );
+    
+    bool foundValidRecord = false;
+    
+    std::copy_if(
+        std::istream_iterator<DataStruct>(iss),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data),
+        [&foundValidRecord](const DataStruct&) {
+            foundValidRecord = true;
+            return true; 
+        }
+    );
+    
+    if (!foundValidRecord)
+    {
+        std::cerr << "Test skipped" << std::endl;
+        return 1;
     }
-);
-
-if (!foundValidRecord)
-{
-    std::cerr << "Test skipped" << std::endl;
-    return 1;
-}
-
-std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1)
-        return a.key1 < b.key1;
-    if (a.key2 != b.key2)
-        return a.key2 < b.key2;
-    return a.key3.length() < b.key3.length();
-});
-
-std::copy(
-    data.begin(),
-    data.end(),
-    std::ostream_iterator<DataStruct>(std::cout, "\n")
-);
-
-return 0;
+    
+    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1)
+            return a.key1 < b.key1;
+        if (a.key2 != b.key2)
+            return a.key2 < b.key2;
+        return a.key3.length() < b.key3.length();
+    });
+    
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+    
+    return 0;
 }
