0fb0d0f608ca954632d934cfd3d0906f7192cb75
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 01:33:51 +0300

    defining operators >> and << on same way-4

--- mungoi.shellerv/T2/data_struct.h
+++ mungoi.shellerv/T2/data_struct.h
@@ -11,6 +11,5 @@ struct DataStruct {
 };
 
 std::istream& operator>>(std::istream& in, DataStruct& data);
-std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 
 #endif // DATA_STRUCT_H
