b04e48eb5e254bc8e0c497597b62f8967b63252a
Виктор Евстигнеев <victorevstigneev36@gmail.com>
2024-05-21 01:10:21 +0300

    mist

--- evstigneev.victor/T3/main.cpp
+++ evstigneev.victor/T3/main.cpp
@@ -11,13 +11,13 @@ int main(int argc, char** argv)
   if (argc != 2)
   {
     std::cerr << "Error: wrong number of parameters\n";
-    return 2;
+    return 1;
   }
   std::ifstream in(argv[1]);
   if (!in)
   {
     std::cerr << "Error: unable to open the file\n";
-    return 2;
+    return 1;
   }
   std::vector<evstigneev::Polygon> poly;
   while (!in.eof())
@@ -27,46 +27,46 @@ int main(int argc, char** argv)
       std::istream_iterator<evstigneev::Polygon>(),
       std::back_inserter(poly)
     );
-    if (in.fail() && !in.eof())
+    if (in.fail())
     {
       in.clear();
       in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
-  std::map<std::string, std::function<std::ostream& (std::istream&, std::ostream&,
-    std::vector<evstigneev::Polygon>&)>> cmd;
-  {
-    cmd["AREA"] = std::bind(evstigneev::area, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-    cmd["MAX"] = std::bind(evstigneev::max, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-    cmd["MIN"] = std::bind(evstigneev::min, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-    cmd["COUNT"] = std::bind(evstigneev::count, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-    cmd["LESSAREA"] = std::bind(evstigneev::lessArea, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-    cmd["MAXSEQ"] = std::bind(evstigneev::maxSeq, std::placeholders::_1,
-      std::placeholders::_2, std::placeholders::_3);
-  }
+  using CMD = std::map<std::string, std::function<std::ostream& (std::istream&, std::ostream&,
+    std::vector<evstigneev::Polygon>&)>>;
+  CMD cmd;
+  cmd["AREA"] = std::bind(evstigneev::area, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+  cmd["MAX"] = std::bind(evstigneev::max, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+  cmd["MIN"] = std::bind(evstigneev::min, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+  cmd["COUNT"] = std::bind(evstigneev::count, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+  cmd["LESSAREA"] = std::bind(evstigneev::lessArea, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+  cmd["MAXSEQ"] = std::bind(evstigneev::maxSeq, std::placeholders::_1,
+    std::placeholders::_2, std::placeholders::_3);
+
   std::string command = "";
+  std::cout << std::setprecision(1) << std::fixed;
   while (std::cin >> command)
   {
+    if (std::cin.eof())
+    {
+      return 0;
+    }
     try
     {
       cmd.at(command)(std::cin, std::cout, poly);
+      std::cout << "\n";
     }
-    catch (const std::out_of_range&)
+    catch (const std::exception&)
     {
       std::cerr << "<INVALID COMMAND>" << "\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-    catch (const std::logic_error&)
-    {
-      std::cerr << "<INVALID COMMAND>" << "\n";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
   return 0;
