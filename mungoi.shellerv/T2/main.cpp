f8bc4927205934638fcf0bc5e536d05942985b46
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 02:04:51 +0300

    defining operators >> and << on same way-9

--- mungoi.shellerv/T2/main.cpp
+++ mungoi.shellerv/T2/main.cpp
@@ -15,25 +15,25 @@ int main()
         "( :key1 10.5d :key2 123ll :key3 \"Date\" )\n"
         "( :key1 45.0d :key2 -123ll :key3 \"Fig\" )\n"
     );
-    
+
     bool foundValidRecord = false;
-    
+
     std::copy_if(
         std::istream_iterator<DataStruct>(iss),
         std::istream_iterator<DataStruct>(),
         std::back_inserter(data),
         [&foundValidRecord](const DataStruct&) {
             foundValidRecord = true;
-            return true; 
+            return true;
         }
     );
-    
+
     if (!foundValidRecord)
     {
         std::cerr << "Test skipped" << std::endl;
         return 1;
     }
-    
+
     std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
         if (a.key1 != b.key1)
             return a.key1 < b.key1;
@@ -41,12 +41,12 @@ int main()
             return a.key2 < b.key2;
         return a.key3.length() < b.key3.length();
     });
-    
+
     std::copy(
         data.begin(),
         data.end(),
         std::ostream_iterator<DataStruct>(std::cout, "\n")
     );
-    
+
     return 0;
 }
