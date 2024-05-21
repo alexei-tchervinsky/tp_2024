dd18e2b815545c5727da5acce9edca52617037ee
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 14:34:03 +0300

    trying new code struct-2

--- mungoi.shellerv/T2/main.cpp
+++ mungoi.shellerv/T2/main.cpp
@@ -8,19 +8,16 @@
 
 int main()
 {
-    std::vector<mungoi::DataStruct> data_struct;
-    while (std::cin.eof() != true)
-    {
-        std::copy(std::istream_iterator<mungoi::DataStruct>(std::cin),
-            std::istream_iterator<mungoi::DataStruct>(),
-            std::back_inserter(data_struct));
-        if (std::cin.fail() == true)
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-    }
+    std::vector<mungoi::DataStruct> data_struct = {
+        {45.0, 123, "Apple"},
+        {10.5, -45, "Banana"},
+        {45.0, 123, "Cherry"},
+        {10.5, 123, "Date"},
+        {45.0, -123, "Fig"}
+    };
+
     std::stable_sort(data_struct.begin(), data_struct.end(), mungoi::Compare());
     std::copy(data_struct.cbegin(), data_struct.cend(), std::ostream_iterator<mungoi::DataStruct>{std::cout, "\n"});
+
     return 0;
 }
