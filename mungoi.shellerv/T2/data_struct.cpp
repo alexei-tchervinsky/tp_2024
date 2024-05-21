71b158ed5c24820f3d8a00abe995be88aa154a24
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 05:34:22 +0300

    restarting-0

--- mungoi.shellerv/T2/data_struct.cpp
+++ mungoi.shellerv/T2/data_struct.cpp
@@ -1,79 +1,117 @@
 #include "data_struct.h"
-#include <sstream>
 #include <iomanip>
-#include <regex>
 
-// Updated parsing logic for operator>>
-std::istream& operator>>(std::istream& in, DataStruct& data)
+namespace mungoi 
 {
-    std::istream::sentry sentry(in);
-    if (!sentry)
+    std::istream& operator>>(std::istream& in, DoubleIO&& dest) 
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        return in >> dest.d;
+    }
+
+    std::istream& operator>>(std::istream& in, LongLongIO&& dest) 
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) 
+        {
+            return in;
+        }
+        return in >> dest.ll;
+    }
+
+    std::istream& operator>>(std::istream& in, StringIO&& dest) 
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        return std::getline(in >> std::ws, dest.str, '"');
+    }
+
+    std::istream& operator>>(std::istream& in, DataStruct& dest) 
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) 
+        {
+            return in;
+        }
+
+        iofmtguard fmtguard(in);
+        DataStruct input;
+        std::string curr;
+        in >> std::ws >> curr;
+
+        if (curr != "(:key1") 
+        {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        in >> DoubleIO{ input.key1 } >> curr;
+        if (curr != ":key2") 
+        {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        in >> LongLongIO{ input.key2 } >> curr;
+        if (curr != ":key3") 
+        {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        in >> std::quoted(input.key3) >> curr;
+        if (curr != ":)") 
+        {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        if (in) 
+        {
+            dest = input;
+        }
         return in;
-
-    std::string input;
-    if (!std::getline(in, input, ')'))
-    {
-        in.setstate(std::ios::failbit);
-        return in;
     }
 
-    std::istringstream iss(input + ')');
-    char c;
-    if (!(iss >> c) || c != '(')
+    std::ostream& operator<<(std::ostream& out, const DataStruct& src) 
     {
-        in.setstate(std::ios::failbit);
-        return in;
-    }
-
-    bool key1_set = false, key2_set = false, key3_set = false;
-    std::string key;
-    double key1_temp;
-    long long key2_temp;
-    std::string key3_temp;
-
-    std::regex key1_pattern(R"(:key1\s*(-?\d+(\.\d*)?([eE][+-]?\d+)?d))");
-    std::regex key2_pattern(R"(:key2\s*(-?\d+ll))");
-    std::regex key3_pattern(R"(:key3\s*"(.*)");
-
-    std::smatch match;
-    if (std::regex_search(input, match, key1_pattern) && match.size() > 1)
-    {
-        key1_temp = std::stod(match.str(1));
-        key1_set = true;
-    }
-    if (std::regex_search(input, match, key2_pattern) && match.size() > 1)
-    {
-        key2_temp = std::stoll(match.str(1));
-        key2_set = true;
-    }
-    if (std::regex_search(input, match, key3_pattern) && match.size() > 1)
-    {
-        key3_temp = match.str(1);
-        key3_set = true;
-    }
-
-    if (key1_set && key2_set && key3_set)
-    {
-        data.key1 = key1_temp;
-        data.key2 = key2_temp;
-        data.key3 = key3_temp;
-    }
-    else
-    {
-        in.setstate(std::ios::failbit);
-    }
-
-    return in;
-}
-
-// Unchanged output operator
-std::ostream& operator<<(std::ostream& out, const DataStruct& data)
-{
-    std::ostream::sentry sentry(out);
-    if (!sentry)
+        std::ostream::sentry sentry(out);
+        if (!sentry) 
+        {
+            return out;
+        }
+        iofmtguard fmtguard(out);
+        out << "(:key1 " << src.key1 << ":key2 " << src.key2 << ":key3 \"" << src.key3 << "\":)";
         return out;
+    }
 
-    out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1 <<
-        "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
-    return out;
+    bool operator<(const DataStruct& a, const DataStruct& b) 
+    {
+        if (a.key1 != b.key1) 
+        {
+            return a.key1 < b.key1;
+        }
+        if (a.key2 != b.key2) 
+        {
+            return a.key2 < b.key2;
+        }
+        return a.key3.size() < b.key3.size();
+    }
+
+    iofmtguard::iofmtguard(std::basic_ios<char>& s) :
+        s_(s),
+        fill_(s.fill()),
+        precision_(s.precision()),
+        fmt_(s.flags()) {}
+
+    iofmtguard::~iofmtguard() {
+        s_.fill(fill_);
+        s_.precision(precision_);
+        s_.flags(fmt_);
+    }
 }
--- mungoi.shellerv/T2/data_struct.h
+++ mungoi.shellerv/T2/data_struct.h
@@ -1,16 +1,49 @@
 #ifndef DATA_STRUCT_H
 #define DATA_STRUCT_H
-
 #include <iostream>
 #include <string>
 
-struct DataStruct {
-    double key1;
-    long long key2;
-    std::string key3;
+namespace mungoi 
+{
+    struct DataStruct 
+    {
+        double key1;
+        long long key2;
+        std::string key3;
+    };
 
-    friend std::istream& operator>>(std::istream& in, DataStruct& data);
-    friend std::ostream& operator<<(std::ostream& out, const DataStruct& data);
-};
+    struct DoubleIO 
+    {
+        double& d;
+    };
+
+    struct LongLongIO 
+    {
+        long long& ll;
+    };
+
+    struct StringIO {
+        std::string& str;
+    };
+
+    std::istream& operator>>(std::istream& in, DoubleIO&& dest);
+    std::istream& operator>>(std::istream& in, LongLongIO&& dest);
+    std::istream& operator>>(std::istream& in, StringIO&& dest);
+    std::istream& operator>>(std::istream& in, DataStruct& dest);
+    std::ostream& operator<<(std::ostream& out, const DataStruct& src);
+    bool operator<(const DataStruct& a, const DataStruct& b);
+
+    class iofmtguard 
+    {
+    public:
+        explicit iofmtguard(std::basic_ios<char>& s);
+        ~iofmtguard();
+    private:
+        std::basic_ios<char>& s_;
+        char fill_;
+        std::streamsize precision_;
+        std::basic_ios<char>::fmtflags fmt_;
+    };
+}
 
 #endif // DATA_STRUCT_H
