3b02450182f8061caa685c3168a159cd342fb9e8
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-21 03:08:59 +0300

    defining operators >> and << on same way-14

--- mungoi.shellerv/T2/data_struct.cpp
+++ mungoi.shellerv/T2/data_struct.cpp
@@ -1,16 +1,14 @@
 #include "data_struct.h"
 #include <sstream>
 #include <iomanip>
+#include <iostream>
 
 std::istream& operator>>(std::istream& in, DataStruct& data)
 {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-        return in;
-
     std::string input;
     if (!std::getline(in, input, ')'))
     {
+        std::cerr << "Error reading line" << std::endl;
         in.setstate(std::ios::failbit);
         return in;
     }
@@ -19,6 +17,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
     char c;
     if (!(iss >> c) || c != '(')
     {
+        std::cerr << "Expected '(', but got: " << c << std::endl;
         in.setstate(std::ios::failbit);
         return in;
     }
@@ -32,6 +31,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             std::string key;
             if (!(iss >> key))
             {
+                std::cerr << "Error reading key" << std::endl;
                 in.setstate(std::ios::failbit);
                 return in;
             }
@@ -40,6 +40,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             {
                 if (!(iss >> data.key1) || !(iss >> c) || c != 'd')
                 {
+                    std::cerr << "Error reading key1" << std::endl;
                     in.setstate(std::ios::failbit);
                     return in;
                 }
@@ -49,6 +50,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             {
                 if (!(iss >> data.key2) || !(iss >> c) || c != 'l' || !(iss >> c) || c != 'l')
                 {
+                    std::cerr << "Error reading key2" << std::endl;
                     in.setstate(std::ios::failbit);
                     return in;
                 }
@@ -58,6 +60,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             {
                 if (!(iss >> std::quoted(data.key3)))
                 {
+                    std::cerr << "Error reading key3" << std::endl;
                     in.setstate(std::ios::failbit);
                     return in;
                 }
@@ -65,6 +68,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
             }
             else
             {
+                std::cerr << "Unknown key: " << key << std::endl;
                 in.setstate(std::ios::failbit);
                 return in;
             }
@@ -76,6 +80,7 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
 
     if (!(key1_set && key2_set && key3_set))
     {
+        std::cerr << "Missing keys in input" << std::endl;
         in.setstate(std::ios::failbit);
     }
 
@@ -84,10 +89,6 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
 
 std::ostream& operator<<(std::ostream& out, const DataStruct& data)
 {
-    std::ostream::sentry sentry(out);
-    if (!sentry)
-        return out;
-
     out << "( :key1 " << std::fixed << std::setprecision(1) << data.key1 <<
         "d :key2 " << data.key2 << "ll :key3 " << std::quoted(data.key3) << " )";
     return out;
