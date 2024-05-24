14b3da92db1825aa3597d5029a12aaa2377e894c
ZwelakheMaseko <zwelakhe.mzwet@gmail.com>
2024-05-24 06:44:46 +0300

    fiing indentation in files

--- maseko.zwelakhe/T2/main.cpp
+++ maseko.zwelakhe/T2/main.cpp
@@ -9,47 +9,45 @@
 #include "nspace.hpp"
 
 
-
 int main()
 {
-    using nspace::Data;
+  using nspace::Data;
 
-    std::vector< Data > data;
-    std::istringstream iss("{ \"key1\": 20D, \"key2\": 89l,\"key3\": \"Just started learning github\" }");
+  std::vector< Data > data;
 
-    while (!std::cin.eof()) {
+  while (!std::cin.eof()) {
     std::copy(
-        std::istream_iterator< Data >(std::cin),
-        std::istream_iterator< Data >(),
-        std::back_inserter(data)
+      std::istream_iterator< Data >(std::cin),
+      std::istream_iterator< Data >(),
+      std::back_inserter(data)
     );
     if (std::cin.fail()) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    }
+  }
     
-    std::sort(data.begin(), data.end(),
-        [](const Data& a, const Data& b) {
-            if (a.key1 != b.key1)
-            {
-                return (a.key1 < b.key1);
-            }
-            if (a.key2 != b.key2)
-            {
-                return (a.key2 < b.key2);
-            }
+  std::sort(data.begin(), data.end(),
+    [](const Data& a, const Data& b) {
+      if (a.key1 != b.key1)
+      {
+        return (a.key1 < b.key1);
+      }
+      if (a.key2 != b.key2)
+      {
+        return (a.key2 < b.key2);
+      }
 
-            return (a.key3.length() < b.key3.length());
-        });
-        
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator< Data >(std::cout, "\n")
-    );
+      return (a.key3.length() < b.key3.length());
+    });
 
-    return 0;
+  std::copy(
+    data.begin(),
+    data.end(),
+    std::ostream_iterator< Data >(std::cout, "\n")
+  );
+
+  return 0;
 }
 
 
