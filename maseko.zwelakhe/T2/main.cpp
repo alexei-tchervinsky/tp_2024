882e36a538997f2f6cd56717e325c5f2ee1f48b4
ZwelakheMaseko <zwelakhe.mzwet@gmail.com>
2024-05-24 06:33:55 +0300

    modifying main.cpp

--- maseko.zwelakhe/T2/main.cpp
+++ maseko.zwelakhe/T2/main.cpp
@@ -49,58 +49,6 @@ int main()
         std::ostream_iterator< Data >(std::cout, "\n")
     );
 
-    /*std::vector< Data > data2;
-    std::istringstream iss2("{ \"key1\": 15D, \"key2\": 21l,\"key3\": \"Git Lab T2\" }");
-
-    std::copy(
-        std::istream_iterator< Data >(iss2),
-        std::istream_iterator< Data >(),
-        std::back_inserter(data2)
-    );
-
-
-    std::vector< Data > data3;
-    std::istringstream iss3("{ \"key1\": 3D, \"key2\": 34l,\"key3\": \"Git Lab T3\" }");
-
-    std::copy(
-        std::istream_iterator< Data >(iss3),
-        std::istream_iterator< Data >(),
-        std::back_inserter(data3)
-    );
-
-
-    std::vector< Data > data4;
-    std::istringstream iss4("{ \"key1\": 3D, \"key2\": 38l,\"key3\": \"Git Lab dublicate\" }");
-
-    std::copy(
-        std::istream_iterator< Data >(iss4),
-        std::istream_iterator< Data >(),
-        std::back_inserter(data4)
-    );
-
-
-
-    std::cout << "\nsorting data items\n";
-    std::vector< Data > DItems[4] = { data, data2, data3, data4 };
-
-    for (size_t i = 0; i < 4; ++i) {
-        std::copy(
-            std::begin(DItems[i]),
-            std::end(DItems[i]),
-            std::ostream_iterator< Data >(std::cout, "\n")
-        );
-    }
-    std::cout << "\n";
-
-    sort(DItems, 4);
-    for (size_t i = 0; i < 4; ++i) {
-        std::copy(
-            std::begin(DItems[i]),
-            std::end(DItems[i]),
-            std::ostream_iterator< Data >(std::cout, "\n")
-        );
-    }*/
-
     return 0;
 }
 
