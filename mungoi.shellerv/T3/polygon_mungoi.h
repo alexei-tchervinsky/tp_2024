0a0519fb1771ee73f841f7e384aee4197e82547b
Sheller Valmiro <shellervalmiro2@gmail.com>
2024-05-26 05:03:36 +0300

    fixing gudline1.11.12

--- mungoi.shellerv/T3/polygon_mungoi.h
+++ mungoi.shellerv/T3/polygon_mungoi.h
@@ -14,20 +14,20 @@
 
 namespace mungoisheller
 {
-	double get_sum(double result, const Polygon& polygon, std::size_t param);
-	double get_area(std::size_t param, const std::vector<Polygon>& polygons);
-	double get_max(std::size_t param, const std::vector<Polygon>& polygons);
-	double get_min(std::size_t param, const std::vector<Polygon>& polygons);
-	std::size_t get_count(std::size_t param, const std::vector<Polygon>& polygons);
-	bool get_rects(const Point& start, const Point& end1, const Point& end2, const Point& end3);
-	std::size_t get_seq(std::vector<Polygon>::const_iterator begin, std::vector<Polygon>::const_iterator end, const Polygon& param);
+    double get_sum(double result, const Polygon& polygon, std::size_t param);
+    double get_area(std::size_t param, const std::vector<Polygon>& polygons);
+    double get_max(std::size_t param, const std::vector<Polygon>& polygons);
+    double get_min(std::size_t param, const std::vector<Polygon>& polygons);
+    std::size_t get_count(std::size_t param, const std::vector<Polygon>& polygons);
+    bool get_rects(const Point& start, const Point& end1, const Point& end2, const Point& end3);
+    std::size_t get_seq(std::vector<Polygon>::const_iterator begin, std::vector<Polygon>::const_iterator end, const Polygon& param);
 
-	void area_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
-	void max_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
-	void min_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
-	void count_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
-	void rects_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
-	void maxseq_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void area_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void max_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void min_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void count_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void rects_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
+    void maxseq_param(const std::vector<Polygon>& polygons, std::istream& in, std::ostream& out);
 }
 
-#endif
+#endif // POLYGON_MUNGOI_H
