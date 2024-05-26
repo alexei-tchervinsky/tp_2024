2dbe5dcbac5d56ca541428000b2d60df9adb58d2
Виктор Евстигнеев <victorevstigneev36@gmail.com>
2024-05-26 18:27:26 +0300

    CG

--- evstigneev.victor/T3/dataStruct.hpp
+++ evstigneev.victor/T3/dataStruct.hpp
@@ -20,6 +20,5 @@ namespace evstigneev
   std::istream& operator>>(std::istream& in, evstigneev::Point& dest);
   std::istream& operator>>(std::istream& in, evstigneev::Polygon& poly);
   bool operator==(const Polygon& fp, const Polygon& sp);
-  
 }
 #endif
