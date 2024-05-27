ab1a5966773bda53e2e8f8930b1df00f7d8b112b
Catalina <berezneva.eka@gmail.com>
2024-05-27 10:32:55 +0300

    Update io.h

--- berezneva.ekaterina/T3/io.h
+++ berezneva.ekaterina/T3/io.h
@@ -9,10 +9,10 @@
 
 namespace berezneva
 {
-  std::istream & operator>>(std::istream &in, Point &point);
-  std::istream & operator>>(std::istream &in, Polygon &polygon);
-  std::ostream & operator<<(std::ostream &out, const Point &point);
-  std::ostream & operator<<(std::ostream &out, const Polygon &polygon);
-  bool isPoint(const std::string &str);
+  std::istream& operator>>(std::istream& in, Point& point);
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+  std::ostream& operator<<(std::ostream& out, const Point& point);
+  std::ostream& operator<<(std::ostream& out, const Polygon& polygon);
+  bool isPoint(const std::string& str);
 }
 #endif
