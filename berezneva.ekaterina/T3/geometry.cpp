11c5914360202326f799b4556ed6e25cc49ad0fb
Catalina <berezneva.eka@gmail.com>
2024-05-27 10:31:32 +0300

    Update geometry.cpp

--- berezneva.ekaterina/T3/geometry.cpp
+++ berezneva.ekaterina/T3/geometry.cpp
@@ -1,6 +1,6 @@
 #include "geometry.h"
 
-bool berezneva::Polygon::contains(const berezneva::Point &pnt) const
+bool berezneva::Polygon::contains(const berezneva::Point& pnt) const
 {
   berezneva::Point fixed = points_[0], first = points_[1];
 
@@ -16,7 +16,7 @@ bool berezneva::Polygon::contains(const berezneva::Point &pnt) const
   );
 }
 
-berezneva::Vector::Vector(const Point &p1, const Point &p2) :
+berezneva::Vector::Vector(const Point& p1, const Point& p2) :
   x_(p2.x_ - p1.x_), y_(p2.y_ - p1.y_), z_(0)
 {
 }
@@ -26,12 +26,12 @@ berezneva::Vector::Vector(int x_, int y_, int z) :
 {
 }
 
-berezneva::Vector berezneva::Vector::operator*(const Vector &v) const
+berezneva::Vector berezneva::Vector::operator*(const Vector& v) const
 {
   return { y_ * v.z_ - z_ * v.y_, z_ * v.x_ - x_ * v.z_, x_ * v.y_ - y_ * v.x_ };
 }
 
-bool berezneva::Triangle::contains(const Point &p) const
+bool berezneva::Triangle::contains(const Point& p) const
 {
   Vector A = Vector(a_, b_) * Vector(a_, p),
     B = Vector(b_, c_) * Vector(b_, p),
@@ -40,14 +40,14 @@ bool berezneva::Triangle::contains(const Point &p) const
   return ((A.getZ() >= 0 && B.getZ() >= 0 && C.getZ() >= 0) || (A.getZ() <= 0 && B.getZ() <= 0 && C.getZ() <= 0));
 }
 
-berezneva::Triangle::Triangle(const Point &a, const Point &b, const Point &c) :
+berezneva::Triangle::Triangle(const Point& a, const Point& b, const Point& c) :
   a_(a),
   b_(b),
   c_(c)
 {
 }
 
-double berezneva::getDistance(const berezneva::Point &p1, const berezneva::Point &p2)
+double berezneva::getDistance(const berezneva::Point& p1, const berezneva::Point& p2)
 {
   int dx = p1.x_ - p2.x_;
   int dy = p1.y_ - p2.y_;
@@ -68,25 +68,25 @@ double berezneva::Polygon::getArea() const
   berezneva::Point fixed = points_[0], first = points_[1];
 
   double area = std::accumulate(
-      points_.begin() + 2,
-      points_.end(),
-      0.0,
-      [fixed, &first](double accumulator, berezneva::Point second)
-      {
-        accumulator += Triangle(fixed, first, second).getArea();
-        first = second;
-        return accumulator;
-      }
+    points_.begin() + 2,
+    points_.end(),
+    0.0,
+    [fixed, &first](double accumulator, berezneva::Point second)
+    {
+      accumulator += Triangle(fixed, first, second).getArea();
+      first = second;
+      return accumulator;
+    }
   );
   return area;
 }
 
-bool berezneva::Polygon::operator<(const Polygon &other) const
+bool berezneva::Polygon::operator<(const Polygon& other) const
 {
   return getArea() < other.getArea();
 }
 
-bool berezneva::Polygon::operator==(const Polygon &other) const
+bool berezneva::Polygon::operator==(const Polygon& other) const
 {
   if (points_.size() == other.points_.size())
   {
@@ -100,28 +100,28 @@ bool berezneva::Polygon::operator==(const Polygon &other) const
   return false;
 }
 
-bool berezneva::Polygon::operator!=(const Polygon &other) const
+bool berezneva::Polygon::operator!=(const Polygon& other) const
 {
   return !(*this == other);
 }
 
-bool berezneva::Point::operator==(const Point &other) const
+bool berezneva::Point::operator==(const Point& other) const
 {
   return x_ == other.x_ && y_ == other.y_;
 }
 
-bool berezneva::Point::operator!=(const Point &other) const
+bool berezneva::Point::operator!=(const Point& other) const
 {
   return !(*this == other);
 }
 
-bool berezneva::Polygon::isIntersect(const Polygon &other) const
+bool berezneva::Polygon::isIntersect(const Polygon& other) const
 {
 
   return std::any_of(
     points_.begin(),
     points_.end(),
-    [&other](const Point &pnt)
+    [&other](const Point& pnt)
     {
       return other.contains(pnt);
     }
