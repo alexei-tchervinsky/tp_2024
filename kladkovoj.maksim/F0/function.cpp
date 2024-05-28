330d41a2ba2a4f9f6c5d6815e54b767496f9ed43
Kladkovoi Maksim <cladkovoj@yandex.ru>
2024-05-28 16:20:16 +0300

    V1.1

--- kladkovoj.maksim/F0/function.cpp
+++ kladkovoj.maksim/F0/function.cpp
@@ -20,11 +20,15 @@ void Knapsack::writeToFile(const std::string& filename) const
 {
   std::ofstream file(filename);
   if (!file.is_open())
+  {
     throw std::invalid_argument("Error opening file for writing.");
+  }
 
   file << maxWeight_ << ' ' << numItems_ << '\n';
   for (size_t i = 0; i < weights_.size(); ++i)
+  {
     file << weights_[i] << ' ' << values_[i] << '\n';
+  }
 
   file.close();
 }
@@ -37,9 +41,13 @@ int Knapsack::knapsackDP() const
     for (int w = 1; w <= maxWeight_; ++w)
     {
       if (weights_[i - 1] <= w)
+      {
         dp[i][w] = std::max(dp[i - 1][w], dp[i - 1][w - weights_[i - 1]] + values_[i - 1]);
+      }
       else
+      {
         dp[i][w] = dp[i - 1][w];
+      }
     }
   }
   return dp[numItems_][maxWeight_];
@@ -53,7 +61,9 @@ int Knapsack::knapsackBacktracking() const
     if (i == numItems_)
     {
       if (currentWeight <= maxWeight_)
+      {
         maxValue = std::max(maxValue, currentValue);
+      }
       return;
     }
     backtrack(i + 1, currentWeight, currentValue);
@@ -76,7 +86,9 @@ int Knapsack::knapsackBranchAndBound() const
   auto bound = [&](const Node& u)
   {
     if (u.weight >= maxWeight_)
+    {
       return 0;
+    }
     int profitBound = u.profit;
     int j = u.level + 1;
     int totWeight = u.weight;
@@ -86,7 +98,10 @@ int Knapsack::knapsackBranchAndBound() const
       profitBound += values_[j];
       j++;
     }
-    if (j < numItems_) profitBound += (maxWeight_ - totWeight) * values_[j] / weights_[j];
+    if (j < numItems_)
+    {
+      profitBound += (maxWeight_ - totWeight) * values_[j] / weights_[j];
+    }
     return profitBound;
   };
 
@@ -103,26 +118,36 @@ int Knapsack::knapsackBranchAndBound() const
     Q.pop_back();
 
     if (u.level == -1)
+    {
       v.level = 0;
+    }
     if (u.level == numItems_ - 1)
+    {
       continue;
+    }
 
     v.level = u.level + 1;
     v.weight = u.weight + weights_[v.level];
     v.profit = u.profit + values_[v.level];
 
     if (v.weight <= maxWeight_ && v.profit > maxProfit)
+    {
       maxProfit = v.profit;
+    }
 
     v.bound = bound(v);
     if (v.bound > maxProfit)
+    {
       Q.push_back(v);
+    }
 
     v.weight = u.weight;
     v.profit = u.profit;
     v.bound = bound(v);
     if (v.bound > maxProfit)
+    {
       Q.push_back(v);
+    }
   }
   return maxProfit;
 }
--- kladkovoj.maksim/F0/function.hpp
+++ kladkovoj.maksim/F0/function.hpp
@@ -8,22 +8,22 @@ namespace kladkovoj
 {
   class Knapsack
   {
-    public:
-      Knapsack(int numItems, int maxWeight);
+  public:
+    Knapsack(int numItems, int maxWeight);
 
-      void addItem(int weight, int value);
-      void writeToFile(const std::string& filename) const;
+    void addItem(int weight, int value);
+    void writeToFile(const std::string& filename) const;
 
-      int knapsackDP() const;
-      int knapsackBacktracking() const;
-      int knapsackBranchAndBound() const;
-      int knapsackBruteForce() const;
+    int knapsackDP() const;
+    int knapsackBacktracking() const;
+    int knapsackBranchAndBound() const;
+    int knapsackBruteForce() const;
 
-    private:
-      int maxWeight_;
-      int numItems_;
-      std::vector<int> weights_;
-      std::vector<int> values_;
+  private:
+    int maxWeight_;
+    int numItems_;
+    std::vector<int> weights_;
+    std::vector<int> values_;
   };
 }
 
