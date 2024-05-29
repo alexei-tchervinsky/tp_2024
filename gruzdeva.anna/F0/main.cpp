0c157b39fe25f1485f7726b4d29cd91fff9e6239
Ann Gruzdeva <theghostlyspirit@outlook.com>
2024-05-29 22:46:47 +0300

    v1.3 - fix: bind(remove) added new argument

--- gruzdeva.anna/F0/main.cpp
+++ gruzdeva.anna/F0/main.cpp
@@ -19,8 +19,8 @@ int main() {
     cmds["LIST"] = std::bind(commands::getAllDictionaries, std::placeholders::_1, std::placeholders::_4);
     cmds["CREATE"] = std::bind(commands::create, std::placeholders::_1, std::placeholders::_2,
                                std::placeholders::_3, std::placeholders::_4);
-    cmds["RENAME"] = std::bind(commands::rename, std::placeholders::_1, std::placeholders::_3,
-                               std::placeholders::_4);
+    cmds["RENAME"] = std::bind(commands::rename, std::placeholders::_1, std::placeholders::_2
+                               std::placeholders::_3, std::placeholders::_4);
     cmds["DELETE"] = std::bind(commands::del, std::placeholders::_1, std::placeholders::_3, std::placeholders::_4);
     cmds["USE"] = std::bind(commands::changeCurrentDictionary, std::placeholders::_1, std::placeholders::_2,
                             std::placeholders::_3, std::placeholders::_4);
