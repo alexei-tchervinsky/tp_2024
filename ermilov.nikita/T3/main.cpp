c7e2409b9974301c82715ded935aeb675150a975
NikitzaInc <5863004@mail.ru>
2024-05-29 19:36:06 +0300

    main.cpp fix

--- ermilov.nikita/T3/main.cpp
+++ ermilov.nikita/T3/main.cpp
@@ -1,8 +1,46 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <limits>
 #include "Functions.h"
 #include "Geometry.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!\n";
+  using namespace ermilov;
+  
+  std::string file = argv[1];
+  std::ifstream input(file);
+  Polygon polygon;
+  std::vector<Polygon> polygons;
+
+  if (!input.is_open())
+  {
+    std::cerr << "BAD FILE" << '\n';
+    return -1;
+  }
+
+  while (!input.eof())
+  {
+    input >> polygon;
+    if (!input.fail()) {
+      polygons.push_back(polygon);
+    }
+    else {
+      input.clear();
+      input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  try
+  {
+    while (!std::cin.eof())
+    {
+      chooseCommand(std::cin, std::cout, polygons);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
 }
