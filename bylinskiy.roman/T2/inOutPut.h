32103f6669a449698e6056aa2519e5518a6ce335
qnderkk <Qndi@yandex.ru>
2024-05-29 20:36:00 +0300

    fix

--- bylinskiy.roman/T2/inOutPut.h
+++ bylinskiy.roman/T2/inOutPut.h
@@ -10,13 +10,13 @@
 #include "dataStruct.h"
 
 namespace bylinskiy {
-	std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-	std::istream& operator>>(std::istream& in, UllBinIO&& dest);
-	std::istream& operator>>(std::istream& in, UllLitIO&& dest);
-	std::istream& operator>>(std::istream& in, StringIO&& dest);
-	std::istream& operator>>(std::istream& in, DelStrIO&& dest);
-	std::istream& operator>>(std::istream& in, DataStruct& dest);
-	std::ostream& operator<<(std::ostream& out, const DataStruct& src);
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, UllBinIO&& dest);
+  std::istream& operator>>(std::istream& in, UllLitIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, DelStrIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src);
 }
 
 #endif
