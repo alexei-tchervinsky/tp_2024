27d520466b2cf27f64bc65566d81c5c4c3d27d6d
4eburekGit <kottsovdan@gmail.com>
2024-05-29 20:41:28 +0300

    ver-1.0-basically-added-everything-from-original-work-but-in-STL

---
+++ kottsov.daniil/F0/inputUIRGR.h
@@ -0,0 +1,544 @@
+#include <iostream>
+#include "GraphRGR.h"
+#include <string>
+
+#ifndef UI
+#define UI
+
+template <typename Weight = int>
+struct NumberIO
+{
+  Weight& ref;
+};
+template <typename Node>
+struct NodeIO
+{
+  Node& ref;
+};
+template <typename Weight = int>
+std::istream& operator>>(std::istream& in, NumberIO<Weight>&& num)
+{
+  std::istream::sentry ward(in);
+  if (!ward)
+  {
+    std::cerr << "istream failure\n";
+    return in;
+  }
+  in >> num.ref;
+  return in;
+}
+template <typename Node>
+std::istream& operator>>(std::istream& in, NodeIO<Node>&& node)
+{
+  std::istream::sentry ward(in);
+  if (!ward)
+  {
+    std::cerr << "istream failure\n";
+    return in;
+  }
+  in >> node.ref;
+  return in;
+}
+template <typename Node, typename Weight = int>
+bool inputUI(Graph<Node, Weight>& graph, std::ostream& out, std::istream& in)
+{
+  std::istream::sentry ward(in);
+  if (!ward && in.eof())
+  {
+    out << "finishing...\n";
+    return 1;
+  }
+  else if (!ward)
+  {
+    std::cerr << "istream failure\n";
+    return 1;
+  }
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    std::cerr << "ostream failure\n";
+    return 1;
+  }
+  using stringIO = NodeIO<std::string>;
+  std::string command = "";
+  in >> stringIO{ command };
+  if (!in)
+  {
+    std::cin.setstate(std::ios::failbit);
+    std::cerr << "ERR. INVALID INPUT\n";
+    return 0;
+  }
+  if (command == "HELP")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    out <<
+      "GET_GRAPH: " << "print graph to standart output\n" <<
+      "ADD_NODE %NAME%: " << "insert a new node with a name %NAME%\n" <<
+      "ADD_BRANCH %NODE_START% %NODE_END%: " << "insert a branch from %NODE_START% to %NODE_END%\n" <<
+      "REM_NODE %NAME%: " << "remove the node with %NAME% name\n" <<
+      "REM_BRANCH %NODE_START% %NODE_END%: " << "remove a branch from %NODE_START% to %NODE_END%\n" <<
+      "EXISTS_NODE %NAME%: " << "returns whether a node with %NAME% name exists\n" <<
+      "EXISTS_BRANCH %NODE_START% %NODE_END%: " << "returns whether a branch exists from %NODE_START% to %NODE_END%\n" <<
+      "REMOVE_CYCLES: " << "removes cycles and parallel branches from the graph\n" <<
+      "GET_DEGREES %NAME%: " << "prints all 3 degrees of the node with %NAME% name\n" <<
+      "GET_MAX_DEG: " << "returns all 3 max degrees in the graph (if 2 or more have the biggest degree returns first occurence)\n" <<
+      "GET_DELTAS: " << "returns nodes that have only incoming branches\n" <<
+      "GET_SOURCES: " << "returns nodes that have only outcoming branches\n" <<
+      "HELP: " << "shows this info\n";
+    out << "DONE\n";
+    return 0;
+  }
+  else if (command == "GET_GRAPH")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      try
+      {
+        graph.print(out);
+        out << "DONE\n";
+      }
+      catch (const std::out_of_range& ex)
+      {
+        std::cerr << "ERR. " << ex.what() << '\n';
+        return 0;
+      }
+    }
+    catch (const std::ios_base::failure& err)
+    {
+      std::cerr << err.what() << '\n';
+      std::cerr << "FATAL ERROR : OSTREAM FAILURE\n";
+      return 1;
+    }
+    return 0;
+  }
+  else if (command == "ADD_NODE")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node name;
+    in >> NodeIO<Node>{ name };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      graph.insertNode(name);
+      out << "DONE\n";
+    }
+    catch (const std::invalid_argument& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "ADD_BRANCH")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node start;
+    Node end;
+    in >> NodeIO<Node>{ start };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    in >> NodeIO<Node>{ end };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      graph.insertBranch(start, end);
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "REM_NODE")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node node;
+    in >> NodeIO<Node>{ node };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      try
+      {
+        graph.removeNode(node);
+        out << "DONE\n";
+      }
+      catch (const std::invalid_argument& err)
+      {
+        std::cerr << "ERR.\n" << err.what() << '\n';
+        std::cin.setstate(std::ios::failbit);
+        return 0;
+      }
+    }
+    catch (const std::out_of_range& ex)
+    {
+      std::cerr << "ERR.\n" << ex.what() << "\n";
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "REM_BRANCH")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node start;
+    Node end;
+    in >> NodeIO<Node>{ start };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    in >> NodeIO<Node>{ end };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      try
+      {
+        graph.removeBranch(start, end);
+        out << "DONE\n";
+      }
+      catch (const std::invalid_argument& ex)
+      {
+        std::cerr << "ERR.\n" << ex.what() << '\n';
+        std::cin.setstate(std::ios::failbit);
+        return 0;
+      }
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "EXISTS_NODE")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node node;
+    in >> NodeIO<Node>{ node };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      if (graph.existsNode(node))
+      {
+        out << "YES\n";
+      }
+      else
+      {
+        out << "NO\n";
+      }
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "EXISTS_BRANCH")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node start;
+    Node end;
+    in >> NodeIO<Node>{ start };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    in >> NodeIO<Node>{ end };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      if (graph.existsBranch(start, end))
+      {
+        out << "YES\n";
+      }
+      else
+      {
+        out << "NO\n";
+      }
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "REMOVE_CYCLES")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      graph.removeCycles();
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "GET_DEGREES")
+  {
+    if (in.peek() == '\n')
+    {
+      std::cerr << "NOT ENOUGH ARGUMENTS\n";
+      return 0;
+    }
+    Node node;
+    in >> NodeIO<Node>{ node };
+    if (!in)
+    {
+      std::cin.setstate(std::ios::failbit);
+      std::cerr << "ERR. INVALID ARGUMENT\n";
+      return 0;
+    }
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      try
+      {
+        out << "total: " << graph.totalDegree(node)
+            << " in: " << graph.inDegree(node)
+            << " out: " << graph.outDegree(node) << '\n';
+        out << "DONE\n";
+      }
+      catch (const std::invalid_argument& ex)
+      {
+        std::cerr << "ERR.\n" << ex.what() << '\n';
+        std::cin.setstate(std::ios::failbit);
+        return 0;
+      }
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "GET_MAX_DEG")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      try
+      {
+        auto a = graph.maxDegree(0);
+        auto b = graph.maxDegree(-1);
+        auto c = graph.maxDegree(1);
+        out << "total: " << std::get<0>(a) << ':' << std::get<1>(a)
+            << " in: " << std::get<0>(b) << ':' << std::get<1>(b)
+            << " out: " << std::get<0>(c) << ':' << std::get<1>(c) << '\n';
+        out << "DONE\n";
+      }
+      catch (const std::invalid_argument& ex)
+      {
+        std::cerr << "ERR.\n" << ex.what() << '\n';
+        std::cin.setstate(std::ios::failbit);
+        return 0;
+      }
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "GET_DELTAS")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      out << "Deltas: ";
+      graph.deltas(out);
+      out << '\n';
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else if (command == "GET_SOURCES")
+  {
+    if (in.peek() != '\n')
+    {
+      std::cerr << "INVALID CMD\n";
+      return 0;
+    }
+    try
+    {
+      out << "Sources: ";
+      graph.sources(out);
+      out << '\n';
+      out << "DONE\n";
+    }
+    catch (const std::out_of_range& err)
+    {
+      std::cerr << "ERR.\n" << err.what() << '\n';
+      std::cin.setstate(std::ios::failbit);
+      return 0;
+    }
+    return 0;
+  }
+  else
+  {
+    std::cerr << "INVALID CMD\n";
+    std::cin.setstate(std::ios::failbit);
+    return 0;
+  }
+  out << "DONE\n";
+  return 0;
+}
+
+#endif
