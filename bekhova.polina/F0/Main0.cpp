f503867599288497e717bf82535cfb458ab206fc
Полина Бехова <behova.pa@edu.spbstu.ru>
2024-05-29 22:17:03 +0300

    F0

---
+++ bekhova.polina/F0/Main0.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <exception>
+#include <functional>
+#include "commands.h"
+
+int main()
+{
+  std::cout << "If you need help, write a 'HELP' \n";
+  std::map<std::string, std::size_t> dict;
+  std::map <std::string,
+    std::function<void(bekhova::dictionary&, std::istream&, std::ostream&)>> cmd;
+  cmd["HELP"] = std::bind(bekhova::help, std::placeholders::_3);
+  cmd["CREATE"] = std::bind(bekhova::create, std::placeholders::_1, std::placeholders::_2,
+    std::placeholders::_3);
+  cmd["TOP"] = std::bind(bekhova::top, std::placeholders::_1, std::placeholders::_2,
+    std::placeholders::_3);
+  cmd["FIND"] = std::bind(bekhova::find, std::placeholders::_1, std::placeholders::_2,
+    std::placeholders::_3);
+  cmd["MERGE"] = std::bind(bekhova::merge, std::placeholders::_1, std::placeholders::_2,
+    std::placeholders::_3);
+  cmd["REMOVE"] = std::bind(bekhova::remove, std::placeholders::_1, std::placeholders::_3);
+  std::string command = "";
+  while (std::cin >> command)
+  {
+    try
+    {
+      cmd.at(command)(dict, std::cin, std::cout);
+    }
+    catch (const std::exception&)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
---
+++ bekhova.polina/F0/commands.h
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_HPP
+#define COMMAND_HPP
+#include <iostream>
+#include <map> 
+
+namespace bekhova
+{
+  using dictionary = std::map<std::string, std::size_t>;
+
+  void help(std::ostream& out);
+  void create(dictionary& dict, std::istream& in, std::ostream& out);
+  void top(dictionary& dict, std::istream& in, std::ostream& out);
+  void find(dictionary& dict, std::istream& in, std::ostream& out);
+  void merge(dictionary& dict, std::istream& in, std::ostream& out);
+  void remove(dictionary& dict, std::ostream& out);
+}
+
+#endif
