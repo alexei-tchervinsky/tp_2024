f43e8c202e449012a47c6396d127970ba6a3517a
Siemens <semzinsemzin@gmail.com>
2024-05-29 15:29:35 +0300

    continue fixing CG 1.3 and 1.5 F0

--- zinakov.semen/F0/main.cpp
+++ zinakov.semen/F0/main.cpp
@@ -7,30 +7,32 @@
 int main()
 {
   std::cout << "--------- Frequency Dictionary ---------\n";
-  std::cout << "Type 'commands' to view list of available commands.\n";
+  std::cout << "Type 'cmds' to view list of available cmds.\n";
 
   using namespace std::placeholders;
   std::unordered_map<std::string, std::size_t> frequencyDict;
 
-  std::unordered_map<std::string, std::function<void(std::unordered_map<std::string, std::size_t> &, std::istream &, std::ostream &)>> commands;
-  commands["commands"] = std::bind(semzin::commands, _3);
-  commands["make"] = std::bind(semzin::make, _1, _2, _3);
-  commands["sortMtoL"] = std::bind(semzin::sortMtoL, _1, _2, _3);
-  commands["sortLtoM"] = std::bind(semzin::sortLtoM, _1, _2, _3);
-  commands["print"] = std::bind(semzin::print, _1, _3);
-  commands["holyTrinityMost"] = std::bind(semzin::holyTrinityMost, _1, _2, _3);
-  commands["holyTrinityLeast"] = std::bind(semzin::holyTrinityLeast, _1, _2, _3);
-  commands["find"] = std::bind(semzin::findWord, _1, _2, _3);
-  commands["remove"] = std::bind(semzin::removeWord, _1, _2, _3);
-  commands["deleteDict"] = std::bind(semzin::remove, _1, _3);
-  commands["save"] = std::bind(semzin::save, _1, _2, _3);
+  std::unordered_map<std::string,
+  std::function<void(std::unordered_map<std::string, std::size_t> &, std::istream &, std::ostream &)>> cmds;
+
+  cmds["commands"] = std::bind(semzin::commands, _3);
+  cmds["make"] = std::bind(semzin::make, _1, _2, _3);
+  cmds["sortMtoL"] = std::bind(semzin::sortMtoL, _1, _2, _3);
+  cmds["sortLtoM"] = std::bind(semzin::sortLtoM, _1, _2, _3);
+  cmds["print"] = std::bind(semzin::print, _1, _3);
+  cmds["holyTrinityMost"] = std::bind(semzin::holyTrinityMost, _1, _2, _3);
+  cmds["holyTrinityLeast"] = std::bind(semzin::holyTrinityLeast, _1, _2, _3);
+  cmds["find"] = std::bind(semzin::findWord, _1, _2, _3);
+  cmds["remove"] = std::bind(semzin::removeWord, _1, _2, _3);
+  cmds["deleteDict"] = std::bind(semzin::remove, _1, _3);
+  cmds["save"] = std::bind(semzin::save, _1, _2, _3);
 
   std::string command = "";
   while (std::cin >> command)
   {
     try
     {
-      commands.at(command)(frequencyDict, std::cin, std::cout);
+      cmds.at(command)(frequencyDict, std::cin, std::cout);
     }
     catch (std::out_of_range &ex)
     {
