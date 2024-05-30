835c1df40614632e99e5f5690f04162c0649b0cd
Ростислав Бредюк <bredyuk.rk@edu.spbstu.ru>
2024-05-30 15:13:12 +0300

    fixfiixfixif

--- bredyuk.rostislav/F0/commands.hpp
+++ bredyuk.rostislav/F0/commands.hpp
@@ -5,20 +5,20 @@
 
 namespace bredyuk
 {
-	struct Node
-	{
-		char ch;
-		int freq = 0;
-		Node* left;
-		Node* right = nullptr;
-	};
-	using huffCode = std::unordered_map<char, std::string>;
+    struct Node
+    {
+        char ch;
+        int freq = 0;
+        Node* left;
+        Node* right = nullptr;
+    };
+    using huffCode = std::unordered_map<char, std::string>;
 
 
 
-	void create(huffCode huffmanCode, std::istream& in, std::ostream& out);
-	void display(huffCode& huffmanCode, std::istream& in, std::ostream& out);
-	void encode(huffCode& huffmanCode, bredyuk::Node* root, std::string str);
+    void create(huffCode huffmanCode, std::istream& in, std::ostream& out);
+    void display(huffCode& huffmanCode, std::istream& in, std::ostream& out);
+    void encode(huffCode& huffmanCode, bredyuk::Node* root, std::string str);
 }
 
 #endif
