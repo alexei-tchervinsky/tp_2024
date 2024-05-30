42f96dc7ff211993ba3eccb1486598837bf7fb11
Ростислав Бредюк <bredyuk.rk@edu.spbstu.ru>
2024-05-30 10:31:15 +0300

    atmpt 2

---
+++ bredyuk.rostislav/T3/main.cpp
@@ -0,0 +1,73 @@
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <limits>
+#include <map>
+#include <functional>
+
+#include "Commands.hpp"
+
+using namespace bredyuk;
+
+int main(int argC, char* argV[])
+{
+    if (argC != 2)
+    {
+        std::cerr << "<INCORRECT USAGE>" << std::endl;
+        return 1;
+    }
+
+    std::string fileName = argV[1];
+
+    std::ifstream file(fileName);
+
+    if (!file)
+    {
+        std::cerr << "<FILE NOT FOUND>" << std::endl;
+        return 1;
+    }
+
+    std::cout << std::setprecision(1) << std::fixed;
+    std::vector<Polygon> data;
+    while (!file.eof())
+    {
+        std::copy(std::istream_iterator<Polygon>(file), std::istream_iterator<Polygon>(), std::back_inserter(data));
+        if (!file.eof() && file.fail())
+        {
+            file.clear();
+            file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    std::map<std::string, std::function<void(const std::vector<Polygon>&)>> commandMap =
+    {
+      {"AREA", command::area},
+      {"MIN", command::min},
+      {"MAX", command::max},
+      {"COUNT", command::count},
+      {"RECTS", command::rects},
+      {"INTERSECTIONS", command::intersections}
+    };
+
+    while (!std::cin.eof())
+    {
+        std::string cmd;
+
+        std::cin >> cmd;
+
+        try
+        {
+            auto it = commandMap.find(cmd);
+            if (it != commandMap.end())
+                it->second(data);
+            else if (!cmd.empty())
+                throw std::invalid_argument("<INVALID COMMAND>");
+        }
+        catch (std::exception& e)
+        {
+            std::cout << e.what() << '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+}
