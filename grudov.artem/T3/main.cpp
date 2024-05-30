abd166ded080aacbb9003461df403ffb2c5ab25b
artem <main.dev@mail.ru>
2024-05-30 15:13:55 +0300

    Output

--- grudov.artem/T3/main.cpp
+++ grudov.artem/T3/main.cpp
@@ -29,21 +29,33 @@ int main(int argc, char* argv[])
     }
   }
 
-  std::map< std::string, std::function< void(const std::vector< grudov::Polygon >, std::istream &, std::ostream &) > > cmds;
+  std::map< std::string, std::function< void(const std::vector< grudov::Polygon >, std::istream &, std::ostream &) > > commands
   {
-    cmds["AREA"] = std::bind(grudov::area, _1, _2, _3);
-    cmds["MAX"] = std::bind(grudov::max, _1, _2, _3);
-    cmds["MIN"] = std::bind(grudov::min, _1, _2, _3);
-    cmds["COUNT"] = std::bind(grudov::count, _1, _2, _3);
-    cmds["RECTS"] = std::bind(grudov::rects, _1, _2, _3);
-    cmds["MAXSEQ"] = std::bind(grudov::maxseq, _1, _2, _3);
+    {"AREA", grudov::area},
+    {"MAX", grudov::max},
+    {"MIN", grudov::min},
+    {"COUNT", grudov::count},
+    {"RECTS", grudov::rects},
+    {"MAXSEQ", grudov::maxseq}
   };
 
-  std::string command = "";
-  while (std::cin >> command) {
-    try {
-      cmds.at(command)(polygons, std::cin, std::cout);
-    }
+  std::cout << std::fixed;
+  while (!std::cin.eof())
+  {
+    try
+    {
+      std::string key;
+      std::cin >> key;
+      if (commands.find(key) != commands.end())
+      {
+        auto command = std::bind(commands[key], std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+        command(polygons, std::cin, std::cout);
+      }
+      else if (!std::cin.eof())
+      {
+        throw std::invalid_argument("<INVALID COMMAND>");
+      }
+        }
     catch (const std::exception& ex)
     {
       std::cout << ex.what() << '\n';
