c9fc782df1acf97abc29e36037f1dc985ff973ec
Lin Zedong <986663534@qq.com>
2024-06-08 23:46:37 +0300

    T2 fix3

--- lin.zedong/T2/main.cpp
+++ lin.zedong/T2/main.cpp
@@ -8,11 +8,23 @@ int main()
 {
     std::vector<namesp::DataStruct> dataStructs;
 
-    std::copy(std::istream_iterator<namesp::DataStruct>(std::cin), std::istream_iterator<namesp::DataStruct>(), std::back_inserter(dataStructs));
+    std::copy(
+        std::istream_iterator<namesp::DataStruct>(std::cin),
+        std::istream_iterator<namesp::DataStruct>(),
+        std::back_inserter(dataStructs)
+        );
 
-    std::sort(dataStructs.begin(), dataStructs.end(), namesp::Compare());
+    std::sort(
+        dataStructs.begin(),
+        dataStructs.end(),
+        namesp::Compare()
+        );
 
-    std::copy(dataStructs.begin(), dataStructs.end(), std::ostream_iterator<namesp::DataStruct>(std::cout, "\n"));
+    std::copy(
+        dataStructs.begin(),
+        dataStructs.end(),
+        std::ostream_iterator<namesp::DataStruct>(std::cout, "\n")
+        );
 
     return 0;
 }
