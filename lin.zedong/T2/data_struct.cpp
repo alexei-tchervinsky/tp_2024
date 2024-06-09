573da04c9ce8d363bd78ea7ef1571252ec3fe724
Lin Zedong <986663534@qq.com>
2024-06-10 00:03:12 +0300

    T2 fix6

--- lin.zedong/T2/data_struct.cpp
+++ lin.zedong/T2/data_struct.cpp
@@ -163,14 +163,6 @@ namespace namesp
 
     bool Compare::operator()(DataStruct first, DataStruct second) const
     {
-        if (first.key1 != second.key1)
-        {
-            return first.key1 < second.key1;
-        }
-        if (first.key2 != second.key2)
-        {
-            return first.key2 < second.key2;
-        }
-        return first.key3.length() < second.key3.length();
+        return first < second;
     }
 }
--- lin.zedong/T2/data_struct.hpp
+++ lin.zedong/T2/data_struct.hpp
@@ -11,6 +11,14 @@ namespace namesp
         double key1;
         long long key2;
         std::string key3;
+        bool operator<(const DataStruct& other) const
+        {
+            if (key1 != other.key1)
+                return key1 < other.key1;
+            if (key2 != other.key2)
+                return key2 < other.key2;
+            return key3 < other.key3;
+        }
     };
 
     struct DelimiterIO
