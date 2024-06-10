8d80753743cf7a76ac2ca7ef6e1a99ac6ff543c9
Lin Zedong <986663534@qq.com>
2024-06-10 23:54:03 +0300

    T2 fix

--- lin.zedong/T2/main.cpp
+++ lin.zedong/T2/main.cpp
@@ -11,23 +11,17 @@ int main()
     using namespace namesp;
     std::vector< DataStruct > data;
     DataStruct input;
-    while (std::cin >> input)
+    while (!std::cin.eof())
     {
-        data.push_back(input);
+        std::copy(std::istream_iterator< DataStruct >{std::cin}, std::istream_iterator< DataStruct >{}, std::back_inserter(data));
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        }
     }
-
-    if (std::cin.fail() && !std::cin.eof())
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-
     std::sort(data.begin(), data.end());
-
-    for (const auto& elem : data)
-    {
-        std::cout << elem << '\n';
-    }
+    std::copy(std::begin(data), std::end(data), std::ostream_iterator< DataStruct >(std::cout, "\n"));
 
     return 0;
 }
