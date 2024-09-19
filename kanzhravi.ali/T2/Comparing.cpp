04458805d48da946e02fb74a0298f092e5be22f4
Ali-Kanjrawi <124464211+Ali-Kanjrawi@users.noreply.github.com>
2024-09-20 02:19:08 +0300

    Update Comparing.cpp

--- kanzhravi.ali/T2/Comparing.cpp
+++ kanzhravi.ali/T2/Comparing.cpp
@@ -1,13 +1,15 @@
-#include "Comparing.hpp"
+#include "Cmp.hpp"
 
 namespace AliKN {
-    bool Comparator::operator()(const DataStruct& first, const DataStruct& second) const {
-        if (first.key1 != second.key1) {
-            return first.key1 < second.key1;
+
+    bool Cmp::operator()(const DataItem first, const DataItem second) const {
+        if (first.attr1 != second.attr1) {
+            return (first.attr1 < second.attr1);
         }
-        else if (first.key2 != second.key2) {
-            return first.key2 < second.key2;
+        else if (first.attr2 != second.attr2) {
+            return (first.attr2 < second.attr2);
         }
-        return first.key3.length() < second.key3.length();
+        return (first.attr3.length() < second.attr3.length());
     }
+
 }
