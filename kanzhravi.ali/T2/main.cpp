c956e252a172a9c89048e174210d263f9f854c71
Ali-Kanjrawi <124464211+Ali-Kanjrawi@users.noreply.github.com>
2024-09-20 02:10:00 +0300

    trying to write a new code in general
    because i forgot the last one and it was wrong anyway

--- kanzhravi.ali/T2/main.cpp
+++ kanzhravi.ali/T2/main.cpp
@@ -1,22 +1,27 @@
-#include "Comparing.hpp"
-#include "DataStruct.hpp"
-#include <algorithm>
+#include "Cmp.hpp"
+#include "DStruct.hpp"
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <vector>
+#include <iterator>
+#include <algorithm>
+#include <bitset>
+#include <limits>
 
 int main() {
-    using namespace AliKN;
-    std::vector<DataStruct> data;
+    using AliKN::DataItem;
+    std::vector<DataItem> records;
+
     while (!std::cin.eof()) {
-        std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
+        std::copy(std::istream_iterator<DataItem>{std::cin}, std::istream_iterator<DataItem>{}, std::back_inserter(records));
+
         if (std::cin.fail()) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-    std::sort(data.begin(), data.end(), Comparator());
-    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
+    std::stable_sort(records.begin(), records.end(), AliKN::Cmp());
+    std::copy(records.cbegin(), records.cend(), std::ostream_iterator<DataItem>(std::cout, "\n"));
+
     return 0;
 }
