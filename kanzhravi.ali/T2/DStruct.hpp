5faec526147f822401ada4994423fe63dd647454
Ali-Kanjrawi <124464211+Ali-Kanjrawi@users.noreply.github.com>
2024-09-20 02:14:47 +0300

    writing a new code already

---
+++ kanzhravi.ali/T2/DStruct.hpp
@@ -0,0 +1,46 @@
+#ifndef DSTRUCT_HPP
+#define DSTRUCT_HPP
+
+#include <iostream>
+#include <string>
+
+namespace AliKN {
+
+    struct DataItem {
+        unsigned long long attr1;
+        unsigned long long attr2;
+        std::string attr3;
+    };
+
+    struct DelimIO {
+        char del;
+    };
+
+    struct LiteralIO {
+        unsigned long long& lit;
+    };
+
+    struct BinaryIO {
+        unsigned long long& bin;
+    };
+
+    struct StrIO {
+        std::string& str;
+    };
+
+    struct LabelIO {
+        std::string label;
+    };
+
+    std::istream& operator>>(std::istream& inStream, DelimIO&& delimiter);
+    std::istream& operator>>(std::istream& inStream, LiteralIO&& literal);
+    std::istream& operator>>(std::istream& inStream, BinaryIO&& binary);
+    std::istream& operator>>(std::istream& inStream, StrIO&& stringData);
+    std::istream& operator>>(std::istream& inStream, LabelIO&& labelData);
+    std::istream& operator>>(std::istream& inStream, DataItem& input);
+    std::ostream& operator<<(std::ostream& outStream, const DataItem& output);
+    std::string toBinary(unsigned long long number);
+
+}
+
+#endif
