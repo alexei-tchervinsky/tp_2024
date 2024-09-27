de9d33b44cc7f02d824ae66f942f1b6ece7c20f6
Ali-Kanjrawi <124464211+Ali-Kanjrawi@users.noreply.github.com>
2024-09-28 02:08:09 +0300

    trying agaaaain

---
+++ kanzhravi.ali/T2/Comparator.cpp
@@ -0,0 +1,17 @@
+#include "Comparator.hpp"
+
+namespace AliKn
+{
+    bool Comparator::operator()(const DataItem first, const DataItem second) const
+    {
+        if (first.attr1 != second.attr1)
+        {
+            return (first.attr1 < second.attr1);
+        }
+        else if (first.attr2 != second.attr2)
+        {
+            return (first.attr2 < second.attr2);
+        }
+        return (first.attr3.length() < second.attr3.length());
+    }
+}
