93ae9a6cfcd7ed11054ed54784d60a33a5afe1e9
Ali-Kanjrawi <124464211+Ali-Kanjrawi@users.noreply.github.com>
2024-09-28 13:27:57 +0300

    last_chance

--- kanzhravi.ali/T3/Commands.hpp
+++ kanzhravi.ali/T3/Commands.hpp
@@ -8,24 +8,24 @@
 namespace AliKn
 {
 	void Area(const std::vector<Polygon>& polygons, std::ostream& out, std::istream& in);
-	void OddArea(const std::vector<Polygon>& polygons, std::ostream& out);
-	void EvenArea(const std::vector<Polygon>& polygons, std::ostream& out);
+	void AreaEven(const std::vector<Polygon>& polygons, std::ostream& out);
+	void AreaOdd(const std::vector<Polygon>& polygons, std::ostream& out);
+	void AreaMean(const std::vector<Polygon>& polygons, std::ostream& out);
 	void AreaVersNum(std::size_t vertexes, const std::vector<Polygon>& polygons, std::ostream& out);
-	void MeanArea(const std::vector<Polygon>& polygons, std::ostream& out);
-	double getArea(const Polygon& polygon);
 	double sumAreas(const std::vector<Polygon>& polygons);
+	double getArea(const Polygon& polygon);
 
-	void maxArea(const std::vector<Polygon>& polygons, std::ostream& out);
 	void Max(const std::vector<Polygon>& polygons, std::ostream& out, std::istream& in);
+	void maxArea(const std::vector<Polygon>& polygons, std::ostream& out);
 	void maxVertexes(const std::vector<Polygon>& polygons, std::ostream& out);
 
 	void Min(const std::vector<Polygon>& polygons, std::ostream& out, std::istream& in);
-	void AreaMin(const std::vector<Polygon>& polygons, std::ostream& out);
-	void MinVertexes(const std::vector<Polygon>& polygons, std::ostream& out);
+	void minArea(const std::vector<Polygon>& polygons, std::ostream& out);
+	void minVertexes(const std::vector<Polygon>& polygons, std::ostream& out);
 
 	void Count(const std::vector<Polygon>& polygons, std::ostream& out, std::istream& in);
-	void CountOdd(const std::vector<Polygon>& polygons, std::ostream& out);
 	void CountEven(const std::vector<Polygon>& polygons, std::ostream& out);
+	void CountOdd(const std::vector<Polygon>& polygons, std::ostream& out);
 	void CountVertexes(std::size_t vertexes, const std::vector<Polygon>& polygons, std::ostream& out);
 
 	void Perms(const std::vector<Polygon>& polygons, std::ostream& out, std::istream& in);
@@ -37,6 +37,6 @@ namespace AliKn
 	Point vectorOnCoords(const Point& firstPoint, const Point& secondPoint);
 	double cosFromVects(const Point& firstPoint, const Point& secondPoint);
 
-	void displayMessage(std::ostream& out, const std::string& message);
+	void outMessage(std::ostream& out, const std::string& message);
 }
 #endif
